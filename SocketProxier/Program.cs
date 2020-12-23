00200b9f0023afcd726366451a44ea71dd40c6e7
Alldis <CAlldis@gb.computacenter.co.uk>
2020-12-23 14:13:36 +0000

    First Version

---
+++ SocketProxier/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketProxier
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int listenPort;
+            IPAddress destinationAddress;
+            int destinationPort;
+            try
+            {
+                listenPort = int.Parse(args[0]);
+                destinationAddress = IPAddress.Parse(args[1]);
+                destinationPort = int.Parse(args[2]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to parse arguments: {e.Message}");
+                Console.WriteLine("Usage: SocketProxier.exe listenPort destinationAddress destinationPort");
+                Console.WriteLine("    Example: SocketProxier.exe 12345 www.webserver.com 443");
+                Console.WriteLine("    (Opens a local listening port on 12345 which redirects traffic to www.webserver.com:443)");
+                return 1;
+            }
+
+            Socket listener;
+            try
+            {
+                listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(new IPEndPoint(IPAddress.Loopback, listenPort));
+                listener.Listen(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to start listener: {e.Message}");
+                return 1;
+            }
+
+            try
+            {
+                while (true)
+                {
+                    var client = listener.Accept();
+                    new SocketProxy(client, destinationAddress, destinationPort);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed during listening loop: {e.Message}");
+                return 1;
+            }
+        }
+    }
+}
---
+++ SocketProxier/SocketProxy.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SocketProxier
+{
+    class SocketProxy : IDisposable
+    {
+        private readonly string _connectionString;
+        private readonly Socket _client;
+        private readonly Socket _server;
+        private long _threadExits = 0;
+        public SocketProxy(Socket client, IPAddress destinationAddress, int destinationPort)
+        {
+            _client = client;
+            _server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _server.Connect(new IPEndPoint(destinationAddress, destinationPort));
+            _connectionString = $"Connection from {client.RemoteEndPoint.ToString()} forwarded through {_server.LocalEndPoint.ToString()} to {_server.RemoteEndPoint.ToString()}";
+            Console.WriteLine($"Open:  {_connectionString}");
+
+            // Launch threads for bi-directional communication
+            Thread serverThread = new Thread(ServerToClient);
+            Thread clientThread = new Thread(ClientToServer);
+            serverThread.Start();
+            clientThread.Start();
+        }
+
+        private void ServerToClient()
+        {
+            byte[] data = new byte[1024];
+            try
+            {
+                while (true)
+                {
+                    int count = _server.Receive(data);
+                    if (count == 0)
+                    {
+                        HandleClosure();
+                        return;
+                    }
+                    _client.Send(data, count, SocketFlags.None);
+                }
+            }
+            catch (Exception e)
+            {
+                HandleError(e);
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+        private void ClientToServer()
+        {
+            byte[] stuff = new byte[1024];
+            try
+            {
+                while (true)
+                {
+                    int count = _client.Receive(stuff);
+                    if (count == 0)
+                    {
+                        HandleClosure();
+                        return;
+                    }
+                    _server.Send(stuff, count, SocketFlags.None);
+                }
+            }
+            catch (Exception e)
+            {
+                HandleError(e);
+            }
+            finally
+            {
+                Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Logs a closed message if no existing exits have been logged
+        /// </summary>
+        private void HandleClosure()
+        {
+            var exits = IncrementExits();
+            if (exits <= 1)
+            {
+                Console.WriteLine($"Close: {_connectionString} closed gracefully");
+            }
+        }
+
+        /// <summary>
+        /// Logs an error if no existing exits have been logged
+        /// </summary>
+        /// <param name="e"></param>
+        private void HandleError(Exception e)
+        {
+            var exits = IncrementExits();
+            if (exits <= 1)
+            {
+                Console.WriteLine($"Error: {_connectionString} closed: {e.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Increments the exit count
+        /// </summary>
+        /// <returns>The new exit count</returns>
+        private long IncrementExits()
+        {
+            Interlocked.Increment(ref _threadExits);
+            return Interlocked.Read(ref _threadExits);
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _server.Close();
+                    _client.Close();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
